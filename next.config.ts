f5ca61a8656ec3f1f9fb85ac7b39a2afa7c3f4bf
Vít Provazník <vit.provaznik.023@pslib.cz>
2026-01-26 22:07:43 +0100

    Add type definition for NextConfig in next.config.ts for improved type safety

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 // next.config.ts
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export', // TENTO ŘÁDEK JE KLÍČOVÝ
   images: {
+    unoptimized: true, // PRO STATICKÝ EXPORT JE TOTO NUTNÉ
     remotePatterns: [
       { protocol: 'https', hostname: 'i.pinimg.com' },
       { protocol: 'https', hostname: 'f3be96abaf.clvaw-cdnwnd.com' },
     ],
   },
 };
+
 export default nextConfig;
